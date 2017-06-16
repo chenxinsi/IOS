f9486722887fcb54d0c95d4c65d29861563dd243
陈心思 <chenxinsi@chenxinsideMacBook-Pro.local>
2017-06-17 00:36:09 +0800

    Initial Commit

---
+++ Test_1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Test_1
+//
+//  Created by 陈心思 on 2017/6/17.
+//  Copyright © 2017年 陈心思. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
