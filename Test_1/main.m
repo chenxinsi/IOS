c6658b99cf6f2b95d6f3bbc09b08cca09e94a4b1
陈心思 <chenxinsi@chenxinsideMacBook-Pro.local>
2017-06-17 10:23:49 +0800

    first commit

--- Test_1/main.m
+++ Test_1/main.m
@@ -8,10 +8,21 @@
 
 #import <Foundation/Foundation.h>
 
+const int WIDTH = 10;
+const int HEIGHT = 5;
+#define NEWLINE 'X';
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World!   \nXinsi!");
+        int a = 0;
+        int b = 0;
+        a = 5;
+        b = 7;
+        int c = WIDTH * HEIGHT;
+        char newline = NEWLINE;
+        NSLog(@"变量c: %d %c",++c,newline);
     }
     return 0;
 }
