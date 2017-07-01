fad895916902a250be52dfe40eea99f3470c1d00
陈心思 <chenxinsi@chenxinsideMacBook-Pro.local>
2017-07-01 18:39:48 +0800

    UILabel

---
+++ Test_UILabel/Test_UILabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_UILabel
+//
+//  Created by 陈心思 on 2017/7/1.
+//  Copyright © 2017年 陈心思. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
